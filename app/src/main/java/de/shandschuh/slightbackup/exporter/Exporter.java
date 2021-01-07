50c3d62c94a83ddc55ca29057f1c39b92da02dc3
UtilsApps <utilsapps2020@gmail.com>
2021-01-07 02:37:18 +0100

    - moved as an Android Studio 4 project - commented out Bookmark parser - now runs in Android SDK 19 - FIXME: problem with running in Android SDK 29: there is no menu button to open the export menu. - TODO: comment out/strip out any code potentially overwriting/deleting call logs or other exportable settings. Turn it into a simple call log exporter.

---
+++ app/src/main/java/de/shandschuh/slightbackup/exporter/Exporter.java
@@ -0,0 +1,216 @@
+/**
+ * Slight backup - a simple backup tool
+ *
+ * Copyright (c) 2011, 2012 Stefan Handschuh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package de.shandschuh.slightbackup.exporter;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Vector;
+
+import android.content.Context;
+import de.shandschuh.slightbackup.BackupActivity;
+import de.shandschuh.slightbackup.R;
+import de.shandschuh.slightbackup.Strings;
+
+public abstract class Exporter {
+	private static final String XML_START = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+	
+	private static final String S_DATE = "s date=\"";
+	
+	private static final String _COUNT = "\" count=\"";
+	
+	protected static final String TAG_END_QUOTE = "\">\n";
+	
+	private static final String TAGS_END = "s>\n";
+	
+	protected static final String ENDTAG_START = "</";
+	
+	protected static final String TAG_END = ">\n";
+	
+	public static class ExporterInfos {
+		public int[] ids;
+		
+		public String[] names;
+		
+		public ExporterInfos(int[] ids, String[] names) {
+			this.ids = ids;
+			this.names = names;
+		}
+		
+		public int length() {
+			return ids != null ? ids.length : 0;
+		}
+		
+	}
+	
+	public static Vector<Class<?>> EXPORTERS;
+	
+	/**
+	 * This is the list of all exporters that are available to this backup
+	 * program.
+	 * Each new exporter has to be added here in order to be recognized by
+	 * this program.
+	 * All menu items and so on are created dynamically.
+	 */
+	static {
+		EXPORTERS = new Vector<Class<?>>();
+
+		//EXPORTERS.add(BookmarkExporter.class);
+		EXPORTERS.add(CallLogExporter.class);
+		EXPORTERS.add(SMSExporter.class);
+		EXPORTERS.add(UserDictionaryExporter.class);
+		EXPORTERS.add(PlaylistExporter.class);
+		EXPORTERS.add(SettingsExporter.class);
+		if (BackupActivity.CANHAVEROOT) {
+			EXPORTERS.add(WifiSettingsExporter.class);
+		}
+		if (BackupActivity.API_LEVEL > 4) {
+			EXPORTERS.add(ContactsExporter.class);
+		}
+		// don't add the "everything"-exporter here
+	}
+	
+	protected ExportTask exportTask;
+	
+	protected boolean canceled;
+	
+	public Exporter(ExportTask exportTask) {
+		this.exportTask = exportTask;
+		canceled = false;
+	}
+	
+	protected abstract int export(String filename) throws Exception;
+	
+	public int export() throws Exception {
+		if (!BackupActivity.DIR.exists() && !BackupActivity.DIR.mkdir()) {
+			throw new Exception(exportTask.getContext().getString(R.string.error_couldnotcreatebackupfolder, BackupActivity.DIR.toString()));
+		}
+		return export(new StringBuilder(BackupActivity.DIR.toString()).append('/').append(getContentName()).append(Strings.FILE_SUFFIX).append(System.currentTimeMillis()).append(Strings.FILE_EXTENSION).toString());
+	}
+	
+	public int getTranslatedContentName() {
+		try {
+			return getClass().getDeclaredField(Strings.FIELD_NAMEID).getInt(null);
+		} catch (Exception e) {
+			return android.R.string.unknownName;
+		}
+	}
+	
+	public String getContentName() {
+		try {
+			return (String) getClass().getDeclaredField(Strings.FIELD_NAME).get(null);
+		} catch (Exception e) {
+			return Strings.EMPTY;
+		}
+	}
+	
+	public int getId() {
+		try {
+			return getClass().getDeclaredField(Strings.FIELD_ID).getInt(null);
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+	
+	public void cancel() {
+		canceled = true;
+	}
+	
+	public abstract String[] getExportedFilenames();
+	
+	public boolean isEncrypted() {
+		return false;
+	}
+	
+	/**
+	 * By overriding this method, the program will show a warning before the
+	 * export that the resulting exported data may be incomplete.
+	 */
+	public boolean maybeIncomplete() {
+		return false;
+	}
+	
+	public String getIncompleteDataNames(Context context) {
+		return context.getString(getTranslatedContentName());
+	}
+	
+	public static Exporter getById(int id, ExportTask exportTask) {
+		if (id == EverythingExporter.ID) {
+			return new EverythingExporter(exportTask);
+		} else {
+			for (Class<?> clazz : EXPORTERS) {
+				try {
+					if (clazz.getDeclaredField(Strings.FIELD_ID).getInt(null) == id) {
+						return (Exporter) clazz.getConstructor(ExportTask.class).newInstance(exportTask);
+					}
+				} catch (Exception e) {
+					
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	public static ExporterInfos getExporterInfos(Context context) {
+		int length = EXPORTERS.size();
+		
+		int ids[] = new int[length];
+		
+		String[] names = new String[length];
+		
+		try {
+			for (int n = 0; n < length; n++) {
+				Class<?> clazz = EXPORTERS.get(n);
+				
+				ids[n] = clazz.getDeclaredField(Strings.FIELD_ID).getInt(null);
+				names[n] = context.getString(clazz.getDeclaredField(Strings.FIELD_NAMEID).getInt(null));
+			}
+		} catch (Exception e) {
+			
+		}
+		
+		return new ExporterInfos(ids, names);
+	}
+	
+	public static void writeXmlStart(Writer writer, String tag, int count) throws IOException {
+		writer.write(XML_START);
+		
+		writer.write('<');
+		writer.write(tag);
+		writer.write(S_DATE);
+		writer.write(Long.toString(System.currentTimeMillis()));
+		writer.write(_COUNT);
+		writer.write(Integer.toString(count));
+		writer.write(TAG_END_QUOTE);
+	}
+	
+	public static void writeXmlEnd(Writer writer, String tag) throws IOException {
+		writer.write(ENDTAG_START);
+		writer.write(tag);
+		writer.write(TAGS_END);
+	}
+
+}
---
+++ app/src/main/java/de/shandschuh/slightbackup/parser/Parser.java
@@ -0,0 +1,170 @@
+/**
+ * Slight backup - a simple backup tool
+ *
+ * Copyright (c) 2011, 2012 Stefan Handschuh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package de.shandschuh.slightbackup.parser;
+
+import java.util.Vector;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+import android.content.Context;
+import de.shandschuh.slightbackup.BackupActivity;
+import de.shandschuh.slightbackup.Strings;
+
+public abstract class Parser extends DefaultHandler {
+	protected static final String COUNT = "count";
+	
+	private static Vector<Class<?>> PARSERS;
+	
+	/**
+	 * This is the list of all importers that are available in this backup
+	 * program.
+	 * Each importer has to be added here to be registered by the this
+	 * program.
+	 * There is a file recognition that tries to find the importer that
+	 * should be used to import a certain file.
+	 */
+	static {
+		PARSERS = new Vector<Class<?>>();
+		
+		//PARSERS.add(BookmarkParser.class);
+		PARSERS.add(CallLogParser.class);
+		PARSERS.add(MessageParser.class);
+		PARSERS.add(PlaylistParser.class);
+		PARSERS.add(SettingsParser.class);
+		PARSERS.add(UserDictionaryParser.class);
+		if (BackupActivity.API_LEVEL > 4) {
+			PARSERS.add(ContactsParser.class);
+		}
+	}
+	
+	protected Context context;
+	
+	protected ImportTask importTask;
+	
+	protected boolean canceled;
+	
+	private StringBuilder hintStringBuilder;
+	
+	private int skipped;
+	
+	private int entryCount;
+	
+	public Parser(Context context, ImportTask importTask) {
+		this.context = context;
+		this.importTask = importTask;
+		skipped = 0;
+		entryCount = 0;
+	}
+
+	public final void cancel() {
+		canceled = true;
+	}
+	
+	public boolean isCanceled() {
+		return canceled;
+	}
+	
+	public static Parser createParserByFilename(String filename, Context context, ImportTask importTask) {
+		filename = filename.substring(filename.lastIndexOf('/')+1);
+		
+		for (Class<?> clazz : PARSERS) {
+			try {
+				if (filename.startsWith((String) clazz.getDeclaredField(Strings.FIELD_NAME).get(null))) {
+					return (Parser) clazz.getConstructor(Context.class, ImportTask.class).newInstance(context, importTask);
+				}
+			} catch (Exception e) {
+				
+			}
+		}
+		return null;
+	}
+
+	public static int getTranslatedParserName(String filename) {
+		filename = filename.substring(filename.lastIndexOf('/')+1);
+		
+		for (Class<?> clazz : PARSERS) {
+			try {
+				if (filename.startsWith((String) clazz.getDeclaredField(Strings.FIELD_NAME).get(null))) {
+					return clazz.getDeclaredField(Strings.FIELD_NAMEID).getInt(null);
+				}
+			} catch (Exception e) {
+				
+			}
+		}
+		return android.R.string.unknownName;
+	}
+	
+	public int getTranslatedContentName() {
+		try {
+			return getClass().getDeclaredField(Strings.FIELD_NAMEID).getInt(null);
+		} catch (Exception e) {
+			return android.R.string.unknownName;
+		}
+	}
+	
+	public void addHint(CharSequence charSequence) {
+		if (hintStringBuilder == null) {
+			hintStringBuilder = new StringBuilder(charSequence);
+		} else {
+			hintStringBuilder.append('\n');
+			hintStringBuilder.append(charSequence);
+		}
+	}
+	
+	public StringBuilder getHints() {
+		return hintStringBuilder;
+	}
+	
+	public boolean hasHints() {
+		return hintStringBuilder != null && hintStringBuilder.length() > 0;
+	}
+	
+	public void addSkippedEntry() {
+		skipped++;
+	}
+	
+	public int getSkippedEntryCount() {
+		return skipped;
+	}
+	
+	protected void setEntryCount(int entryCount) {
+		this.entryCount = entryCount;
+	}
+	
+	public int getEntryCount() {
+		return entryCount;
+	}
+
+	/**
+	 * By overriding this method and returning "true", the program will give
+	 * a warning before the import that the data that should be imported,
+	 * may be incomplete.
+	 */
+	public boolean maybeIncomplete() {
+		return false;
+	}
+
+}
